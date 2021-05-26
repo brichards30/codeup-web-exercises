6b3690e55fca12b91041727f1786398e99cfa7bd
Brittany Richards <brittany.m.richards3@gmail.com>
2021-05-26 15:12:27 -0500

    Javascript Exercisesincomplete

--- js/external.js
+++ js/external.js
@@ -1,1 +1,10 @@
 
+console.log("Hello from external JavaScript");
+
+alert("Welcome to my Website!");
+
+var response = prompt("What is your favorite color?")
+
+alert("Great, " + response + " is my favorite color too!");
+
+
