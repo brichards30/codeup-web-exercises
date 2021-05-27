f15d26403059fbf81cecff99541cb8ee15520c04
Brittany Richards <brittany.m.richards3@gmail.com>
2021-05-27 14:17:26 -0500

    Completed #3 1-3

--- js/external.js
+++ js/external.js
@@ -1,7 +1,7 @@
 
 "use strict";
 
-console.log("Hello from external JavaScript");
+/* console.log("Hello from external JavaScript");
 
 alert("Welcome to my Website!");
 
@@ -9,8 +9,27 @@ var response = prompt("What is your favorite color?")
 
 alert("Great, " + response + " is my favorite color too!");
 
-/* var confirmed = confirm("Are you of age?");
+ var confirmed = confirm("Are you of serious right now?");
 console.log(confirmed); */
+
+/*var price = 3
+var ltm = 3, brother = 5, herc = 1;
+console.log("The Little Mermaid " + (ltm * price));
+console.log("Brother Bear " + (brother * price));
+console.log("Hercules " + (herc * price));
+var total_price = (ltm + brother + herc) * 3;
+console.log(total_price);
+alert("The price for the movies is: " + (3+5+1) * 3);
+
+alert("I got paid " + ((350*10) + (400*6) + (380*4)));*/
+
+var full = confirm("Is this class full?");
+var conflict = confirm("Does this conflict with your schedule?");
+alert("Enrollment Status " + (!full && !conflict));
+
+
+
+
 
 
 
