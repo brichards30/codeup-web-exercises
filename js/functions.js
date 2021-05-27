de17912304e149598b56d3ee545d14917afa1d8d
Brittany Richards <brittany.m.richards3@gmail.com>
2021-05-27 15:22:45 -0500

    2 TODOS complete

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,5 @@
 "use strict";
-
 /**
- * TODO:
  * Create a function called 'sayHello' that takes a parameter 'name'.
  * When called, the function should return a message that says hello to the passed in name.
  *
@@ -9,14 +7,17 @@
  * > sayHello("codeup") // returns "Hello, codeup!"
  */
 
+function sayHello(name) {
+    return "Hello, " + name + "!";
+}
 /**
- * TODO:
  * Call the function 'sayHello' and pass your name as a string literal argument.
  * Store the result of the function call in a variable named 'helloMessage'.
  *
  * console.log 'helloMessage' to check your work
  */
-
+var helloMessage = sayHello("Brittany");
+console.log(helloMessage);
 /**
  * TODO:
  * Store your name as a string in a variable named 'myName', and pass that
