80baae5d43ac861eb1f4849f3cfe966cb3f0e8cc
Brittany Richards <brittany.m.richards3@gmail.com>
2021-05-27 11:59:46 -0500

    before lunchtime edits

--- js/external.js
+++ js/external.js
@@ -9,8 +9,9 @@ var response = prompt("What is your favorite color?")
 
 alert("Great, " + response + " is my favorite color too!");
 
-var confirmed = confirm("Are you of age?");
-console.log(confirmed);
+/* var confirmed = confirm("Are you of age?");
+console.log(confirmed); */
+
 
 
 
