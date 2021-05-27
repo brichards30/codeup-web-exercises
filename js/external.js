334a0376fb8b860b8f22ae6cf7ee7a4537030fd6
Brittany Richards <brittany.m.richards3@gmail.com>
2021-05-27 11:47:53 -0500

    working through during class

--- js/external.js
+++ js/external.js
@@ -1,4 +1,6 @@
 
+"use strict";
+
 console.log("Hello from external JavaScript");
 
 alert("Welcome to my Website!");
@@ -6,5 +8,9 @@ alert("Welcome to my Website!");
 var response = prompt("What is your favorite color?")
 
 alert("Great, " + response + " is my favorite color too!");
+
+var confirmed = confirm("Are you of age?");
+console.log(confirmed);
+
 
 
