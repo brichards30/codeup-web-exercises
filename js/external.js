25f3de8275988c9bed715dfa10b026f6bd977b3c
Brittany Richards <brittany.m.richards3@gmail.com>
2021-05-31 13:20:01 -0500

    JS with HTML & Functions complete

--- js/external.js
+++ js/external.js
@@ -1,7 +1,7 @@
 
 "use strict";
 
-/* console.log("Hello from external JavaScript");
+console.log("Hello from external JavaScript");
 
 alert("Welcome to my Website!");
 
@@ -10,9 +10,9 @@ var response = prompt("What is your favorite color?")
 alert("Great, " + response + " is my favorite color too!");
 
  var confirmed = confirm("Are you of serious right now?");
-console.log(confirmed); */
+console.log(confirmed);
 
-/*var price = 3
+var price = 3
 var ltm = 3, brother = 5, herc = 1;
 console.log("The Little Mermaid " + (ltm * price));
 console.log("Brother Bear " + (brother * price));
@@ -21,17 +21,17 @@ var total_price = (ltm + brother + herc) * 3;
 console.log(total_price);
 alert("The price for the movies is: " + (3+5+1) * 3);
 
-alert("I got paid " + ((350*10) + (400*6) + (380*4)));*/
+alert("I got paid " + ((350*10) + (400*6) + (380*4)));
 
-/*var full = confirm("Is this class full?");
+var full = confirm("Is this class full?");
 var conflict = confirm("Does this conflict with your schedule?");
 alert("Enrollment Status " + (!full && !conflict));*/
 
-/*var premium = confirm("Are you a premium member?");
+*var premium = confirm("Are you a premium member?");
 var quantity = confirm("Are you buying 2 or more items?");
 var offer = confirm("Has the offer expired?");
 
-alert("Offer Status " + (!offer && (quantity || premium)));*/
+alert("Offer Status " + (!offer && (quantity || premium)));
 
 var username = 'codeup';
 var password = 'notastrongpassword';
