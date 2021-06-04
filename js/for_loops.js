8382d5b828a4c515ed279f0d36db418aa75ea2c3
Brittany Richards <brittany.m.richards3@gmail.com>
2021-06-04 11:03:40 -0500

    For Loops Exercise 3

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,19 @@
-// let number = "";
+/*
 function showMultiplicationTable(x,y) {
     for (var y = 0; y <= 10; y++) {
         console.log(x + " x " + y + " = " + (x * y) + "\n");
-
     }
 }
-showMultiplicationTable(7);
+showMultiplicationTable(7);*/
+
+
+for (x = 0; x <10; x++) {
+    let x = Math.ceil(Math.random() * 100) + 10;
+    if (x % 2 === 0) {
+       console.log(x + " is even");
+   } else{
+       console.log(x+ " is odd");
+
+   }
+}
 
