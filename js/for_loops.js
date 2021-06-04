37742252aaed25b2ff8a882d2731d4ce86ab7653
Brittany Richards <brittany.m.richards3@gmail.com>
2021-06-04 11:53:03 -0500

    Exercise 4/5 complete

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,12 +1,13 @@
 /*
+//Exercise 2
 function showMultiplicationTable(x,y) {
     for (var y = 0; y <= 10; y++) {
         console.log(x + " x " + y + " = " + (x * y) + "\n");
     }
 }
-showMultiplicationTable(7);*/
+showMultiplicationTable(7);
 
-
+// Exercise 3
 for (x = 0; x <10; x++) {
     let x = Math.ceil(Math.random() * 100) + 10;
     if (x % 2 === 0) {
@@ -16,4 +17,34 @@ for (x = 0; x <10; x++) {
 
    }
 }
+ */
+
+//E4
+/*
+    for (let x = 1; x < 10; x++) {
+        var number = "";
+        number = x;
+        for (let y = 1; y < x; y++){
+            number = number + x.toString();
+        }
+        console.log(number.toString());
+    }
+
+    for (let i = 1; i < 6; i++) {
+        var branches = "";
+        branches = i;
+        for (let k = 1; k < i; k++) {
+            branches = branches + i.toString();
+        }
+        console.log(branches.toString());
+    }
+
+ */
+
+ //E5
+/* for (let x = 100; x > 0; x = x - 5) {
+    console.log(x);
+}
+ */
+
 
