8c486ecc62076daa19952d9f1d70e2a6fe36d478
Brittany Richards <brittany.m.richards3@gmail.com>
2021-06-07 09:32:00 -0500

    Did an extra exercise

--- js/while.js
+++ js/while.js
@@ -13,7 +13,7 @@ if (number > 67000) {
 
 
 //Do While Loop
-let allCones = Math.floor(Math.random() * 50) + 50;
+/* let allCones = Math.floor(Math.random() * 50) + 50;
 console.log("The number of cones I can sell " + allCones);
 
 //total cones sold today
@@ -28,7 +28,30 @@ do {
     console.log("My customer purchased " + conesBought);
     totalSold = totalSold + conesBought;
     console.log("I've sold " + totalSold + " ice cream cones");
+    console.log("Yay! I sold them all!");
 
 } while (totalSold < allCones);
+ */
+
+let allCars = Math.floor(Math.random() * 50) + 50;
+console.log("I need to sell " + allCars + " cars for my bonus.");
+
+let totalSold = 0;
+
+do {
+    let carsBought = Math.floor(Math.random() * 5) + 1;
+
+    if((totalSold + carsBought) > allCars) {
+        console.log("I'm sorry, I cannot sell " + carsBought);
+        continue;
+    }
+        console.log("My customer purchased " + carsBought + ".");
+        totalSold = totalSold + carsBought;
+        console.log("I've sold " + totalSold + " cars.");
+
+    } while (totalSold < allCars);
+console.log("Yay! I get the bonus!");
+
+
 
 
