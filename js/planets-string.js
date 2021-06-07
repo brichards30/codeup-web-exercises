e59e36595052379e6af7f703b57a884ba6482bcd
Brittany Richards <brittany.m.richards3@gmail.com>
2021-06-07 16:04:49 -0500

    planets-string in progress

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,3 +1,5 @@
+//alert("This is a test.");
+
 (function(){
     "use strict";
 
@@ -5,23 +7,37 @@
     var planetsArray;
 
     /**
-     * TODO:
      * Convert planetsString to an array, and save it in a variable named
      * planetsArray.
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+        planetsArray = planetsString.split("|");
+
+        var planetsString = planetsArray.join(" <br>");
+     console.log(planetsString);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+    var planetsListed = planetsArray.join(" <br>");
+    console.log(planetsListed);
+
+    document.write(planetsListed);
+
+
+
+
+
+
+    //var exploded = lines.split('<br />');
+
+
 
 })();
