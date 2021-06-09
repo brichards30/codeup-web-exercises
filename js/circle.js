213d95e60ba455402a0cbb408751860a993ee24c
Brittany Richards <brittany.m.richards3@gmail.com>
2021-06-09 18:13:32 -0500

    Need to change radius to 5

--- js/circle.js
+++ js/circle.js
@@ -36,17 +36,18 @@
             // log info about the circle
             console.log("Raw circle information");
             console.log("Area of a circle with radius: " + circle.radius + ", is: " + area);
-            console.log(circle.logInfo(false));
-            console.log("Circle information rounded to the nearest whole number");
-            console.log(circle.logInfo(true));
+            //console.log(circle.logInfo(false));
+            console.log("Circle information rounded to the nearest whole number: " + circle.logInfo(true));
+            //console.log(circle.logInfo(true));
 
             console.log("=======================================================");
             // TODO: Change the radius of the circle to 5.
 
             // log info about the circle
-            console.log("Raw circle information");
-            console.log(circle.logInfo(false));
-            console.log("Circle information rounded to the nearest whole number " + circle.logInfo(true));
+            console.log("Raw circle information ");
+            console.log("Area of a circle with radius: " + circle.radius + ", is: " + area);
+            //console.log(circle.logInfo(false));
+            console.log("Circle information rounded to the nearest whole number: " + circle.logInfo(true));
             //console.log(circle.logInfo(true));
 
 //console.log(circle.getArea());
