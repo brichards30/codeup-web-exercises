bf035a9215491bd9c614c4e4c1e5462ade09131c
Brittany Richards <brittany.m.richards3@gmail.com>
2021-07-06 09:31:27 -0500

    couldn't get to work and did not understand Google search result

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -13,3 +13,23 @@ console.log(countSpaces(5));
 console.log(countSpaces(true));
 console.log(countSpaces(null));
 console.log(countSpaces("B Richards"));
+
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+
+function capitalizeAllNames(input) {
+    var input = ["bob","Seth","TOFU","Toyota"];
+    return input.toUpperCase();
+    }
+
+console.log(capitalizeAllNames(input));
+
+
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+
+function capitalizeFirstLetter(input) {
+
+}
